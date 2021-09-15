43f266733c4c4b581fc124d7c7e2928b13276548
marcello.mari@singularitynet.io <marcello.mari@singularitynet.io>
2021-09-15 11:33:13 +0100

    User infos  + transfer counter mappings

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,20 +2,23 @@ import {
   Approval as ApprovalEvent,
   DelegateChanged as DelegateChangedEvent,
   DelegateVotesChanged as DelegateVotesChangedEvent,
-  OwnerChanged as OwnerChangedEvent,
-  OwnerNominated as OwnerNominatedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   Transfer as TransferEvent
 } from "../generated/SingDao/SingDao"
+
+import {
+  User,
+  UserCounter,
+  TransferCounter
+} from './types/schema'
+
 import {
   Approval,
   DelegateChanged,
   DelegateVotesChanged,
-  OwnerChanged,
-  OwnerNominated,
-  OwnershipTransferred,
-  Transfer
+  Transfer,
+  Entity
 } from "../generated/schema"
+
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
@@ -49,33 +52,6 @@ export function handleDelegateVotesChanged(
   entity.save()
 }
 
-export function handleOwnerChanged(event: OwnerChangedEvent): void {
-  let entity = new OwnerChanged(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.oldOwner = event.params.oldOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
-export function handleOwnerNominated(event: OwnerNominatedEvent): void {
-  let entity = new OwnerNominated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
@@ -85,4 +61,57 @@ export function handleTransfer(event: TransferEvent): void {
   entity.to = event.params.to
   entity.amount = event.params.amount
   entity.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let day = (event.block.timestamp / BigInt.fromI32(60 * 60 * 24))
+
+  let userFrom = User.load(event.params.from.toHex())
+  if (userFrom == null) {
+    userFrom = newUser(event.params.from.toHex(), event.params.from.toHex());
+  }
+  userFrom.balance = userFrom.balance - event.params.value
+  userFrom.transactionCount = userFrom.transactionCount + 1
+  userFrom.save()
+
+  let userTo = User.load(event.params.to.toHex())
+  if (userTo == null) {
+    userTo = newUser(event.params.to.toHex(), event.params.to.toHex());
+
+    // UserCounter
+    let userCounter = UserCounter.load('singleton')
+    if (userCounter == null) {
+      userCounter = new UserCounter('singleton')
+      userCounter.count = 1
+    } else {
+      userCounter.count = userCounter.count + 1
+    }
+    userCounter.save()
+    userCounter.id = day.toString()
+    userCounter.save()
+  }
+  userTo.balance = userTo.balance + event.params.value
+  userTo.transactionCount = userTo.transactionCount + 1
+  userTo.save()
+
+  // Transfer counter total and historical
+  let transferCounter = TransferCounter.load('singleton')
+  if (transferCounter == null) {
+    transferCounter = new TransferCounter('singleton')
+    transferCounter.count = 0
+    transferCounter.totalTransferred = BigInt.fromI32(0)
+  }
+  transferCounter.count = transferCounter.count + 1
+  transferCounter.totalTransferred = transferCounter.totalTransferred + event.params.value
+  transferCounter.save()
+  transferCounter.id = day.toString()
+  transferCounter.save()
+}
+
+function newUser(id: string, address: string): User {
+  let user = new User(id);
+  user.address = address
+  user.balance = BigInt.fromI32(0)
+  user.transactionCount = 0
+  return user
 }
